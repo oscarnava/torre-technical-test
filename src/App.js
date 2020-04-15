b8878179da0bfdd8a82ed3c18157712d27c3ab5b
Oscar Nava Trujillo <oscarnava@gmail.com>
2020-04-15 16:50:17 -0500

    Add user name and photo; style it.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,79 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
-import fetchUserInfo from './services/userInfo';
+import { fetchUserRecs, fetchUserInfo } from './services/userInfo';
+import './styles/app.sass';
 
 const randomByte = () => Math.floor(255 * Math.random());
 const generateColor = () => `rgba(${randomByte()},${randomByte()},${randomByte()},1.0)`;
-
-const DATA = {
-  datasets: [{
-    data: [10, 20, 30],
-    backgroundColor: (chart, dataIndex, dataset) => {
-      const color = generateColor();
-      console.log('backgroundColor', color);
-      return color;
-    }
-    ,
-  }],
-  // These labels appear in the legend and in the tooltips when hovering different arcs
-  labels: [
-    'Red',
-    'Yellow',
-    'Blue',
-  ],
-};
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      chartData: DATA,
+      user: 'ana-maria-diaz',
+      chartData: null,
+      person: null,
     };
   }
 
   componentDidMount() {
-    fetchUserInfo('ana-maria-diaz').then((info) => {
-      console.log('componentDidMount', info);
+    const { user } = this.state;
 
-      const { chartData } = this.state;
-      const { data } = chartData.datasets[0];
-      const { labels } = chartData;
+    fetchUserInfo(user).then(({ person }) => {
+      console.log({ person });
+      this.setState({ person });
+    });
 
-      data.length = 0;
-      labels.length = 0;
+    fetchUserRecs(user).then(({ stats }) => {
+      console.log('componentDidMount', stats);
 
-      Object.keys(info.stats).forEach((key) => {
+      const data = [];
+      const labels = [];
+      const keys = Object.keys(stats).sort((a, b) => stats[b] - stats[a]);
+
+      keys.forEach((key) => {
         labels.push(key);
-        data.push(info.stats[key]);
-        return chartData;
+        data.push(Math.round(stats[key]));
       });
-      this.setState({ chartData });
+
+      this.setState({
+        chartData: {
+          datasets: [{
+            data,
+            backgroundColor: generateColor,
+          }],
+          labels,
+        },
+      });
     });
   }
 
+  get contents() {
+    const { chartData, person } = this.state;
+    if (chartData) {
+      const { name, picture } = person || { name: '', picture: '' };
+      return (
+        <div className="recommendations-container">
+          <div className="user-container">
+            <img src={picture} alt={name} />
+            <h1>{name}</h1>
+          </div>
+          <div className="graph-container">
+            <h2>Accumulated recommendations by weight</h2>
+            <Doughnut data={chartData} options={{ maintainAspectRatio: false }} />
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="loading-msg">Loading data...</div>
+    );
+  }
+
   render() {
-    const { chartData } = this.state;
     return (
       <div className="App">
-        <h1>Hello world</h1>
-        <Doughnut
-          data={chartData}
-          width={100}
-          height={100}
-          options={{ maintainAspectRatio: false }}
-        />
+        {this.contents}
       </div>
     );
   }
