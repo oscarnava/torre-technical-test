7a85bd463df717af1c755603cfbfde8db534c6db
Oscar Nava Trujillo <oscarnava@gmail.com>
2020-04-15 17:32:10 -0500

    Fix: Fixed initial random colors & fetch user info

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { fetchUserRecs, fetchUserInfo } from './services/userInfo';
 import './styles/app.sass';
 
 const randomByte = () => Math.floor(255 * Math.random());
-const generateColor = () => `rgba(${randomByte()},${randomByte()},${randomByte()},1.0)`;
+const randomColors = Array(100).fill(0).map(() => `rgba(${randomByte()},${randomByte()},${randomByte()},1.0)`);
+
+const generateColor = ({ dataIndex }) => randomColors[dataIndex];
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: 'ana-maria-diaz',
+      user: 'ana-maria-diaz', // 'manolo',
       chartData: null,
       person: null,
     };
@@ -22,28 +24,28 @@ export default class App extends React.Component {
     fetchUserInfo(user).then(({ person }) => {
       console.log({ person });
       this.setState({ person });
-    });
 
-    fetchUserRecs(user).then(({ stats }) => {
-      console.log('componentDidMount', stats);
+      fetchUserRecs(user).then(({ stats }) => {
+        console.log('componentDidMount', stats);
 
-      const data = [];
-      const labels = [];
-      const keys = Object.keys(stats).sort((a, b) => stats[b] - stats[a]);
+        const data = [];
+        const labels = [];
+        const keys = Object.keys(stats).sort((a, b) => stats[b] - stats[a]);
 
-      keys.forEach((key) => {
-        labels.push(key);
-        data.push(Math.round(stats[key]));
-      });
+        keys.forEach((key) => {
+          labels.push(key);
+          data.push(Math.round(stats[key]));
+        });
 
-      this.setState({
-        chartData: {
-          datasets: [{
-            data,
-            backgroundColor: generateColor,
-          }],
-          labels,
-        },
+        this.setState({
+          chartData: {
+            datasets: [{
+              data,
+              backgroundColor: generateColor,
+            }],
+            labels,
+          },
+        });
       });
     });
   }
@@ -60,6 +62,11 @@ export default class App extends React.Component {
           </div>
           <div className="graph-container">
             <h2>Accumulated recommendations by weight</h2>
+            <p>
+              This graph shows the user&apos;s strengths in a graph weighted by who did the recommendation
+              and the weight of the person&apos;s recommendation. This way we can have a quick image of the
+              best-recommended strengths of the user considering the total weight of them.
+            </p>
             <Doughnut data={chartData} options={{ maintainAspectRatio: false }} />
           </div>
         </div>
