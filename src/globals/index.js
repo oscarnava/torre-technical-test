db53068a6b66b5c91679ba6de7bd49c9b576805f
Oscar Nava Trujillo <oscarnava@gmail.com>
2020-04-15 16:46:25 -0500

    Add mock data for user info

--- src/globals/index.js
+++ src/globals/index.js
@@ -1,3 +1,4 @@
 export default {
-  SERVICE_URL: 'https://bio.torre.co/api/bios',
+  RECS_SERVICE_URL: 'https://bio.torre.co/api/bios',
+  INFO_SERVICE_URL: 'https://torre.bio/api/bios',
 };
--- src/services/mockAPI.js
+++ src/services/mockAPI.js
@@ -1,10 +1,12 @@
 import MOCK_RECS_DATA from './mockRecsData';
+import MOCK_USER_INFO from './mockUserInfo';
 
 const MOCK_DATA = {
   'https://bio.torre.co/api/bios/ana-maria-diaz/recommendations': MOCK_RECS_DATA,
+  'https://torre.bio/api/bios/ana-maria-diaz': [MOCK_USER_INFO],
 };
 
-const request = (url, body) => {
+const request = (url) => {
   const pageInfo = url.match(/(.*)\?page=(\d)/);
   const key = pageInfo ? pageInfo[1] : url;
   const page = pageInfo ? +pageInfo[2] : 0;
