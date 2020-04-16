c4d6a2084aa6477e6b3e578bca4630566a170db8
Oscar Nava Trujillo <oscarnava@gmail.com>
2020-04-15 19:34:13 -0500

    Display message when user doesn't exists

--- src/App.js
+++ src/App.js
@@ -33,19 +33,24 @@ export default class App extends React.Component {
 
   get contents() {
     const { chartData, person, user } = this.state;
+
+    const inputForm = (
+      <div className="user-input-container">
+        <form>
+          <label htmlFor="public-id">
+            User public id:
+            <input id="public-id" type="text" defaultValue={user} ref={this.inputReference} />
+          </label>
+          <button type="submit" onClick={this.onFetch}>Fetch</button>
+        </form>
+      </div>
+    );
+
     if (chartData) {
       const { name, picture } = person || { name: '', picture: '' };
       return (
         <div className="recommendations-container">
-          <div className="user-input-container">
-            <form>
-              <label htmlFor="public-id">
-                User public id:
-                <input id="public-id" type="text" defaultValue={user} ref={this.inputReference} />
-              </label>
-              <button type="submit" onClick={this.onFetch}>Fetch</button>
-            </form>
-          </div>
+          {inputForm}
           <div className="user-container">
             <img src={picture} alt={name} />
             <h1>{name}</h1>
@@ -62,6 +67,18 @@ export default class App extends React.Component {
         </div>
       );
     }
+
+    if (person === '?') {
+      return (
+        <div className="recommendations-container">
+          {inputForm}
+          <div className="user-container">
+            <h1>{`User ${user} not found!`}</h1>
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div className="loading-msg">Loading data...</div>
     );
@@ -70,7 +87,11 @@ export default class App extends React.Component {
   fetchUser(user) {
     fetchUserInfo(user).then(({ person }) => {
       console.log({ person });
-      this.fetchRecs(person);
+      if (person) {
+        this.fetchRecs(person);
+      } else {
+        this.setState({ person: '?', chartData: null });
+      }
     });
   }
 
